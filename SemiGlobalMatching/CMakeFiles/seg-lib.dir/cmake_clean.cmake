5db5f5c9e5a374fc6ae587881195af6e66c7ec25
1113870631 <1113870631@qq.com>
2022-01-11 22:24:56 +0800

    first commit

---
+++ SemiGlobalMatching/CMakeFiles/seg-lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seg-lib.dir/SemiGlobalMatching.cpp.o"
+  "libseg-lib.pdb"
+  "libseg-lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seg-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SemiGlobalMatching/CMakeFiles/sgm-lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgm-lib.dir/sgm_util.cpp.o"
+  "libsgm-lib.pdb"
+  "libsgm-lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sgm-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
